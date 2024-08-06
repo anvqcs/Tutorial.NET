8299943d0e30c7379e892dd21fbf450507c6e5be
anvqcs <anvq@gmail.com>
2024-08-06 21:41:22 +0700

    anvqcs: add new project

---
+++ Tutorial.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tutorial.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
