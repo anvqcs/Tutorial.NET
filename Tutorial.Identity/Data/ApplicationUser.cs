c0a9d09d6850e65a16dfaf5d099ed73e986aa0f7
anvqcs <anvq@gmail.com>
2024-08-06 22:45:45 +0700

    anvqcs:

---
+++ Tutorial.Identity/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(256)]
+        public string FirstName { get; set; } = string.Empty;
+        [MaxLength(256)]
+        public string LastName { get; set; } = string.Empty;
+    }
+}
--- Tutorial.Identity/Program.cs
+++ Tutorial.Identity/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Tutorial.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("IdentityCore") ?? throw new InvalidOperationException("Connection string 'IdentityCore' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString)
+ );
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
